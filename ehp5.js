ab4fd46961a47c8940e6fbfcfd282c19dc45af2c
Evan Piermont <evan.piermont@gmail.com>
2019-04-29 23:43:08 -0400

    new papers

--- ehp5.js
+++ ehp5.js
@@ -5,7 +5,7 @@ background_int = function() {
    h = $(window).height();
    w = $(window).width();
    fs = Math.min(w/2,h);
-   fontSize = fs * .1;
+   fontSize = fs * .08;
    $('#background').css('height', h); //background is exactly window size
    $("body").css('font-size', fontSize).css('height', h); //body is twice window size (allows for scrolling)
 };
