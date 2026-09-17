6471bbe15f83f1359389918e08eabdc01cce6753
Evan Piermont <evan.piermont@gmail.com>
2026-09-17 12:10:41 +0100

    auto_update

---
+++ coauthors-data.js
@@ -0,0 +1,24 @@
+// Co-author directory for evanpiermont.github.io
+//
+// Maps a co-author's name (as it appears in papers-data.js `authors` arrays)
+// to their personal website. Leave the value as an empty string "" if the
+// co-author has no site to link to -- their name will then render as plain
+// text instead of a clickable button.
+//
+// This file must load before ehp5.js, which reads the `coauthors` variable
+// defined here.
+
+var coauthors = {
+  "Araujo, Felipe A.": "https://www.araujofa.com/",
+  "Guerdjikova, Ani": "https://sites.google.com/site/aniguerdjikova/home",
+  "Halpern, Joseph Y.": "https://www.cs.cornell.edu/home/halpern/",
+  "Payró, Fernando": "https://sites.google.com/site/ferpayrochew/home",
+  "Pivato, Marcus": "https://sites.google.com/site/marcuspivato/home",
+  "Quiggin, John": "https://johnquiggin.com/",
+  "Sebastian Lleras, Juan": "",
+  "Svoboda, Richard": "",
+  "Takeoka, Norio": "https://sites.google.com/r.hit-u.ac.jp/noriotakeoka/",
+  "Teper, Roee": "",
+  "Vierø, Marie-Louise": "https://sites.google.com/view/marie-louise",
+  "Zuazo-Garin, Peio": "https://sites.google.com/view/peiozuazo-garin/home"
+};
